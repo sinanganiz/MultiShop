5694f7a738b8780fa622078aa9e80ddfe3b7f14d
Sinan Ganiz <sinan.ganiz@gmail.com>
2025-04-21 23:40:52 +0300

    MVC UI added

---
+++ Frontends/MultiShop.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult _UILayout()
+        {
+            return View();
+        }
+
+    }
+}
