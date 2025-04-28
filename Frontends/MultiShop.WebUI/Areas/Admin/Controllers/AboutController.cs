81dc5f5b6bf1ad5a27ed8cd35f05d672eaa1cb86
Sinan Ganiz <sinan.ganiz@gmail.com>
2025-04-28 14:26:18 +0300

    About feature added to UI and admin panel

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,127 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.AboutDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/About")]
+    public class AboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Hakkımda";
+            ViewBag.v3 = "Hakkımda Listesi";
+            ViewBag.v0 = "Hakkımda İşlemleri";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Abouts");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateAbout")]
+        public IActionResult CreateAbout()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Hakkımda";
+            ViewBag.v3 = "Yeni Hakkımda";
+            ViewBag.v0 = "Yeni Hakkımda";
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateAbout")]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAboutDto);
+
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PostAsync("https://localhost:7070/api/Abouts", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "About", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+        [Route("DeleteAbout/{id}")]
+        public async Task<IActionResult> DeleteAbout(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7070/api/Abouts?id=" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "About", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+
+        [Route("UpdateAbout/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(string id)
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Hakkımda";
+            ViewBag.v3 = "Hakkımda Güncelle";
+            ViewBag.v0 = "Hakkımda Güncelle";
+
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Abouts/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [Route("UpdateAbout/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAbout)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateAbout);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7070/api/Abouts/", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "About", new { area = "Admin" });
+            }
+
+            return View();
+        }
+    }
+}
--- Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.AboutDtos;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Abouts");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+
+                return View(values);
+            }
             return View();
         }
     }
