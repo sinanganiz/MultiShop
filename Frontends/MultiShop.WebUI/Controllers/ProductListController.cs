550ca012d74a44fee8ad2d377d38356a3577ebc7
Sinan Ganiz <sinan.ganiz@gmail.com>
2025-05-05 12:04:32 +0300

    add comment

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index(string id)
         {
             ViewBag.i = id;
@@ -14,6 +25,36 @@ namespace MultiShop.WebUI.Controllers
         public IActionResult ProductDetail(string id)
         {
             ViewBag.x = id;
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CreateCommentDto createCommentDto)
+        {
+            createCommentDto.ImageUrl = "";
+            createCommentDto.Rating = 5;
+            createCommentDto.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            createCommentDto.Status = false;
+            createCommentDto.ProductId = "";
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCommentDto);
+
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PostAsync("https://localhost:7041/api/Comments", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+
             return View();
         }
     }
